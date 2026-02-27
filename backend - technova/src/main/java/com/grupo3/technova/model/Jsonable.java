9d07dbe49860b85a0787b9ceeb3f11d514e2867f
YoDarkyy <77863732+YoDarkyy@users.noreply.github.com>
2026-02-27 12:21:19 +0100

    docs: añadir comentarios (Jsonable)

--- backend - technova/src/main/java/com/grupo3/technova/model/Jsonable.java
+++ backend - technova/src/main/java/com/grupo3/technova/model/Jsonable.java
@@ -2,6 +2,7 @@ package com.grupo3.technova.model;
 
 import com.google.gson.JsonObject;
 
+// Interfaz que obliga a todas las clases que la implementen a tener un método toJsonObject().
 public interface Jsonable {
     JsonObject toJsonObject();
 }
