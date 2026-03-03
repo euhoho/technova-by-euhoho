0e3cd27212cbeea4d79f622947230c2139472d08
YoDarkyy <77863732+YoDarkyy@users.noreply.github.com>
2026-03-03 09:21:34 +0100

    feature: añadir enumCategoría

---
+++ backend - technova/src/main/java/com/grupo3/technova/model/enums/EnumCategoria.java
@@ -0,0 +1,10 @@
+package com.grupo3.technova.model.enums;
+
+// Enum que representa las categorías posibles de un producto.
+// Al usar un enum en vez de un String libre, el compilador nos avisa si escribimos mal un rol
+public enum EnumCategoria {
+    PERIFERICOS,
+    COMPONENTES,
+    REDES,
+    SOFTWARE
+}
